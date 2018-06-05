2e324101d1e4e0f364272da6f9c67ee1b8a8b527
Sergio Sanz <sergio.sanzsacristan@edu.uah.es>
2018-06-05 11:03:58 +0200

    Juego

---
+++ DangerFlask/src/VideojuegoEstados/Sonido.java
@@ -0,0 +1,44 @@
+package VideojuegoEstados;
+
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
+
+/**
+ *
+ * @author SergioSanzSacristan
+ */
+public class Sonido {
+
+    private Sound puerta, tarjeta, julian, zacarias, adrenalina;
+
+    public Sonido() {
+    }
+
+    public void iniciarSonidos() throws SlickException {
+        puerta = new Sound("./Sonidos/abrir_puerta.ogg");
+        tarjeta = new Sound("./Sonidos/coger_tarjeta.ogg");
+        julian = new Sound("./Sonidos/julian_muerte.ogg");
+        zacarias = new Sound("./Sonidos/zacarias_muerte.ogg");
+        adrenalina = new Sound("./Sonidos/usar_adrenalina.ogg");
+    }
+
+    public Sound getPuerta() {
+        return puerta;
+    }
+
+    public Sound getTarjeta() {
+        return tarjeta;
+    }
+
+    public Sound getJulian() {
+        return julian;
+    }
+
+    public Sound getZacarias() {
+        return zacarias;
+    }
+
+    public Sound getAdrenalina() {
+        return adrenalina;
+    }
+}
