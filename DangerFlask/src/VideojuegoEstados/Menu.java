d3885e09eb84756181257ee0c72fac6838325b4d
Sergio Sanz <sergio.sanzsacristan@edu.uah.es>
2018-06-08 11:12:26 +0200

    Juego

--- DangerFlask/src/VideojuegoEstados/Menu.java
+++ DangerFlask/src/VideojuegoEstados/Menu.java
@@ -8,9 +8,7 @@ package VideojuegoEstados;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -21,7 +19,6 @@ import org.newdawn.slick.state.StateBasedGame;
 public class Menu extends BasicGameState {
 
     private Image fondo, jugar, controles, opciones;
-    private Rectangle rectJugar;
 
     public Menu() {
     }
@@ -32,7 +29,6 @@ public class Menu extends BasicGameState {
         jugar = new Image("./Botones/ButtonPlay.png");
         controles = new Image("./Botones/ButtonControls.png");
         opciones = new Image("./Botones/ButtonOptions.png");
-        rectJugar = new Rectangle(105, 522, 283, 69);
     }
 
     @Override
@@ -41,18 +37,17 @@ public class Menu extends BasicGameState {
         jugar.draw(105, 522);
         controles.draw(465, 522);
         opciones.draw(820, 522);
-        g.draw(rectJugar);
-        g.drawString("X: " + container.getInput().getMouseX() + " Y: " + container.getInput().getMouseY(), 10, 10);
+        //g.drawString("X: " + container.getInput().getMouseX() + " Y: " + container.getInput().getMouseY(), 10, 10);
     }
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-//        if (container.getInput().isMouseButtonDown(Input.KEY_LEFT)) {
-//            if (container.getInput().getMouseX() < 388 && container.getInput().getMouseX() > 105
-//                    && container.getInput().getMouseY() < 591 && container.getInput().getMouseY() > 522) {
-//                game.enterState(5);
-//            }
-//        }
+        if (container.getInput().isMouseButtonDown(0)) {
+            if (container.getInput().getMouseX() < 388 && container.getInput().getMouseX() > 105
+                    && container.getInput().getMouseY() < 591 && container.getInput().getMouseY() > 522) {
+                game.enterState(5);
+            }
+        }
     }
 
     @Override
