e0ef4df18c03795952834794f94f37f1c67e3cdf
Sergio Sanz <sergio.sanzsacristan@edu.uah.es>
2018-06-11 09:36:03 +0200

    Juego

--- DangerFlask/src/VideojuegoEstados/Dialogo.java
+++ DangerFlask/src/VideojuegoEstados/Dialogo.java
@@ -34,9 +34,10 @@ public class Dialogo extends BasicGameState {
         g.resetTransform();
         g.drawRect(170, 130, 875, 145);
         g.drawRect(140, 370, 875, 145);
+        g.drawString("X: " + container.getInput().getMouseX() + " Y: " + container.getInput().getMouseY(), 10, 10);
         g.drawString("JULIAN:", 180, 140);
         g.drawString("ZACARIAS:", 150, 380);
-        g.drawString("Pulse ENTER para avanzar", 10, 10);
+        //g.drawString("Pulse ENTER para avanzar", 10, 10);
         if (i == 1) {
             g.drawString("Julián, la cámara.", 150, 400);
         }
@@ -62,12 +63,34 @@ public class Dialogo extends BasicGameState {
             g.drawString("Hecho.", 180, 160);
         }
         if (i == 9) {
-            g.drawString("TEXTO9", 150, 400);
+            g.scale(2, 2);
+            g.drawString("PASAN 3 HORAS", 240, 150);
+            g.resetTransform();
         }
         if (i == 10) {
-            g.drawString("TEXTO10", 180, 160);
+            g.drawString("Doctor, ya han pasado las tres horas. Voy a por la cámara.", 180, 160);
         }
-        if (i == 11){
+        if (i == 11) {
+            g.drawString("Ni te molestes, Julián. Va a ser grabar otro fracaso.", 150, 400);
+        }
+        if (i == 12) {
+            g.drawString("¿Qué demonios dices, doctor?  Basta. No aguanto tu actitud, ¿se te ha olvidado el peso que \nacarreamos? ¿Se te ha olvidado Leo?", 180, 160);
+        }
+        if (i == 13) {
+            g.drawString("¡No vuelvas a mencionar a mi hijo! ¡Claro que no se me ha olvidado, pero es que esto no nos \nestá llevando a ningún lado! Todo lo que estuve investigando años antes no me ha estado \nsirviendo para nada.", 150, 400);
+        }
+        if (i == 14) {
+            g.drawString("Entiendo cómo te sientes, cada día que no encontramos la cura para esta maldita peste, es un \ndía más que siento que voy a perder a mi madre. Pero, tenemos que aguantar la presión...\npor favor.", 180, 160);
+        }
+        if (i == 15) {
+            g.drawString("Lo sé, lo sé. Lo siento, Julián. No te preocupes por mí. Veamos los resultados.", 150, 400);
+        }
+        if (i == 16) {
+            g.scale(2, 2);
+            g.drawString("VAN A VER LOS RESULTADOS", 200, 150);
+            g.resetTransform();
+        }        
+        if (i == 17) {
             game.enterState(0);
         }
         //CUANDO EL DIALOGO ESTE ESCRITO SE TERMINARA
