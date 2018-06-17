46fee94cc64d195a1d53631c2a936a68b8012659
Sergio Sanz Sacristan <sergio.sanzsacristan@edu.uah.es>
2018-06-17 11:20:28 +0200

    Juego

--- DangerFlask/src/VideojuegoEstados/Sonido.java
+++ DangerFlask/src/VideojuegoEstados/Sonido.java
@@ -11,20 +11,17 @@ import org.newdawn.slick.Sound;
  */
 public class Sonido {
 
+    //Atributos
+    //Inicia los atributos de los sonidos
     private Sound puerta, tarjeta, juliandead, zacariasdead, adrenalina;
+    //Si estan a true significa que el sonido y la música estan activados
     private boolean sonidoOn = true, musicaOn = true;
 
+    //Constructor
     public Sonido() {
     }
 
-    public void iniciarSonidos() throws SlickException {
-        puerta = new Sound("./Sonidos/abrir_puerta.ogg");
-        tarjeta = new Sound("./Sonidos/coger_tarjeta.ogg");
-        juliandead = new Sound("./Sonidos/julian_muerte.ogg");
-        zacariasdead = new Sound("./Sonidos/zacarias_muerte.ogg");
-        adrenalina = new Sound("./Sonidos/usar_adrenalina.ogg");
-    }
-
+    //Métodos Get y Set necesarios
     public Sound getPuerta() {
         return puerta;
     }
@@ -60,7 +57,18 @@ public class Sonido {
     public void setMusicaOn(boolean musicaOn) {
         this.musicaOn = musicaOn;
     }
+    
+    //Este método inicia los sonidos
+    public void iniciarSonidos() throws SlickException {
+        puerta = new Sound("./Sonidos/abrir_puerta.ogg");
+        tarjeta = new Sound("./Sonidos/coger_tarjeta.ogg");
+        juliandead = new Sound("./Sonidos/julian_muerte.ogg");
+        zacariasdead = new Sound("./Sonidos/zacarias_muerte.ogg");
+        adrenalina = new Sound("./Sonidos/usar_adrenalina.ogg");
+    }
 
+    //Este método detecta si en los mapas has clickado en activar/desactivar música y sonido, y cambia...
+    //...las variables de musicaOn y sonidoOn si es necesario
     public void click(GameContainer container, Music musica) {
         if (container.getInput().getMouseX() < 657 && container.getInput().getMouseX() > 625
                 && container.getInput().getMouseY() < 32 && container.getInput().getMouseY() > 5) {
