bc22242d5b8712a9515ba6612c08bd09348309a9
Sergio Sanz Sacristan <sergio.sanzsacristan@edu.uah.es>
2018-06-17 10:33:20 +0200

    Juego

--- DangerFlask/src/VideojuegoEstados/Colisiones.java
+++ DangerFlask/src/VideojuegoEstados/Colisiones.java
@@ -8,14 +8,17 @@ import org.newdawn.slick.geom.Rectangle;
  */
 public class Colisiones {
 
+    //Atributos
     private float x_, y_;
     private int tarjeta1 = 0, tarjeta2 = 0, tarjeta3 = 0, tarjeta4 = 0;
 
+    //Constructor
     public Colisiones(float x, float y) {
         this.x_ = x;
         this.y_ = y;
     }
 
+    //Gets y Sets necesarios
     public int getTarjeta1() {
         return tarjeta1;
     }
@@ -48,20 +51,27 @@ public class Colisiones {
         this.tarjeta4 = tarjeta4;
     }
 
-    private Rectangle rectSalida1 = new Rectangle(1150, 495, 55, 30);
-    private Rectangle rectSalida2 = new Rectangle(1167, 270, 50, 25);
-    private Rectangle rectSalida3 = new Rectangle(335, 592, 50, 50);
-    private Rectangle rectSalida4 = new Rectangle(478, 592, 50, 50);
-    private Rectangle cambiarMapa1 = new Rectangle(1213, 499, 1, 45);
-    private Rectangle cambiarMapa2 = new Rectangle(1213, 275, 1, 45);
-    private Rectangle cambiarMapa3 = new Rectangle(336, 638, 45, 1);
-    private Rectangle cambiarMapa4 = new Rectangle(480, 638, 45, 1);
-    private Rectangle rectAnim = new Rectangle(x_ + 2, y_ + 2, 12, 23);
+    //Iniciamos rectángulos que nos servirán para gestionar los cambios de estado...
+    //...y las puertas de cada mapa, tambien el rectangulo que rodea al personaje...
+    //...y asi poder gestionar las colisiones del mismo.
+    private final Rectangle rectSalida1 = new Rectangle(1150, 495, 55, 30);
+    private final Rectangle rectSalida2 = new Rectangle(1167, 270, 50, 25);
+    private final Rectangle rectSalida3 = new Rectangle(335, 592, 50, 50);
+    private final Rectangle rectSalida4 = new Rectangle(478, 592, 50, 50);
+    private final Rectangle cambiarMapa1 = new Rectangle(1213, 499, 1, 45);
+    private final Rectangle cambiarMapa2 = new Rectangle(1213, 275, 1, 45);
+    private final Rectangle cambiarMapa3 = new Rectangle(336, 638, 45, 1);
+    private final Rectangle cambiarMapa4 = new Rectangle(480, 638, 45, 1);
+    private final Rectangle rectAnim = new Rectangle(x_ + 2, y_ + 2, 12, 23);
 
+    //Método get del rectangulo del personaje
     public Rectangle getRectAnim() {
         return rectAnim;
     }
 
+    //Gestionamos las colisiones con las paredes del mapa1 y cuando tiene las...
+    //...dos tarjetas necesarias para poder atravesar la puerta. Devuelve true...
+    //...cuando el personaje colisiona con una pared.
     public boolean animDentro1(boolean[][] paredes, float x, float y) {
         if (((paredes[(int) ((x * 2) / 16)][(int) ((y * 2) / 16) + 3] == true)
                 || (paredes[(int) ((x * 2) / 16) + 2][(int) ((y * 2) / 16) + 3] == true)) && rectAnim.intersects(rectSalida1)
@@ -75,6 +85,9 @@ public class Colisiones {
         }
     }
 
+    //Gestionamos las colisiones con las paredes del mapa2 y cuando tiene las...
+    //...dos tarjetas necesarias para poder atravesar la puerta. Devuelve true...
+    //...cuando el personaje colisiona con una pared.
     public boolean animDentro2(boolean[][] paredes, float x, float y) {
         if (((paredes[(int) ((x * 2) / 16)][(int) ((y * 2) / 16) + 3] == true)
                 || (paredes[(int) ((x * 2) / 16) + 2][(int) ((y * 2) / 16) + 3] == true)) && rectAnim.intersects(rectSalida2)
@@ -88,6 +101,9 @@ public class Colisiones {
         }
     }
 
+    //Gestionamos las colisiones con las paredes del mapa3 y cuando tiene las...
+    //...dos tarjetas necesarias para poder atravesar la puerta. Devuelve true...
+    //...cuando el personaje colisiona con una pared.
     public boolean animDentro3(boolean[][] paredes, float x, float y) {
         if (((paredes[(int) ((x * 2) / 16)][(int) ((y * 2) / 16) + 3] == true)
                 || (paredes[(int) ((x * 2) / 16) + 2][(int) ((y * 2) / 16) + 3] == true)) && rectAnim.intersects(rectSalida3)
@@ -101,6 +117,9 @@ public class Colisiones {
         }
     }
 
+    //Gestionamos las colisiones con las paredes del mapa4 y cuando tiene las...
+    //...dos tarjetas necesarias para poder atravesar la puerta. Devuelve true...
+    //...cuando el personaje colisiona con una pared.
     public boolean animDentro4(boolean[][] paredes, float x, float y) {
         if (((paredes[(int) ((x * 2) / 16)][(int) ((y * 2) / 16) + 3] == true)
                 || (paredes[(int) ((x * 2) / 16) + 2][(int) ((y * 2) / 16) + 3] == true)) && rectAnim.intersects(rectSalida4)
@@ -114,11 +133,14 @@ public class Colisiones {
         }
     }
 
+    //Actualiza la posicion del rectangulo del personaje en funcion de las...
+    //... coordenadas del propio personaje, para mantener la gestion de colisiones
     public void actualizar(float x, float y) {
         rectAnim.setX(x + 2);
         rectAnim.setY(y + 2);
     }
 
+    //Este método se encarga de detectar si el personaje atraviesa la puerta del mapa1
     public boolean cambiarMapa1() {
         if (rectAnim.intersects(cambiarMapa1)) {
             return true;
@@ -127,6 +149,7 @@ public class Colisiones {
         }
     }
 
+    //Este método se encarga de detectar si el personaje atraviesa la puerta del mapa2
     public boolean cambiarMapa2() {
         if (rectAnim.intersects(cambiarMapa2)) {
             return true;
@@ -135,6 +158,7 @@ public class Colisiones {
         }
     }
 
+    //Este método se encarga de detectar si el personaje atraviesa la puerta del mapa3
     public boolean cambiarMapa3() {
         if (rectAnim.intersects(cambiarMapa3)) {
             return true;
@@ -143,6 +167,7 @@ public class Colisiones {
         }
     }
 
+    //Este método se encarga de detectar si el personaje atraviesa la puerta del mapa4
     public boolean cambiarMapa4() {
         if (rectAnim.intersects(cambiarMapa4)) {
             return true;
