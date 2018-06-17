d38db44af66e64aa7ef80eb5eea7f55a6cb867ce
Sergio Sanz Sacristan <sergio.sanzsacristan@edu.uah.es>
2018-06-17 10:13:47 +0200

    Juego

--- DangerFlask/src/VideojuegoEstados/Controles.java
+++ DangerFlask/src/VideojuegoEstados/Controles.java
@@ -1,5 +1,6 @@
 package VideojuegoEstados;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -14,29 +15,38 @@ import org.newdawn.slick.state.StateBasedGame;
  */
 public class Controles extends BasicGameState {
 
+    //Atributos
     private Image controles;
 
+    //Constructor
     public Controles() {
     }
 
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
+        //Iniciamos la imagen
         controles = new Image("./Botones/Controles.png");
     }
 
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+        //Dibujamos la imagen
         controles.draw(0, 0);
+
+        //Instrucciones a seguir en este estado
+        g.setColor(Color.black);
         g.drawString("Pulse ESC para volver al menú.", 10, 10);
     }
 
     @Override
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+        //Si pulsamos ESC nos manda al estado 0
         if (container.getInput().isKeyDown(Input.KEY_ESCAPE)) {
             game.enterState(0);
         }
     }
 
+    //Este estado es el estado 8
     @Override
     public int getID() {
         return 8;
